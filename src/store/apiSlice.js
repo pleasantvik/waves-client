7e30eaa601571561dc86aae2be907218938091d9
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-12 05:05:54 +0100

    build

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getTokenCookie } from "utils/tools";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://pleasantvik-waves.up.railway.app/api",
+  baseUrl: "/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     // const token = getState().auth.token;
